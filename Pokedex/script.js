fd6255d1f1a59f1641ff534005a49cd2da53abb9
Pramit git <pramitsarkarclg@gmail.com>
2025-08-30 22:59:18 +0530

    Added weight, height and interactive cursor

--- Pokedex/script.js
+++ Pokedex/script.js
@@ -30,8 +30,56 @@ async function fetchData() {
         shinysprite.src = shiny;
         shinysprite.style.display = "block";
 
+        const weight = data.weight;
+        const weightelement = document.getElementById("weight")
+        weightelement.textContent = `Weight: ${weight}`;
+
+        const height = data.height;
+        const heightelement = document.getElementById("height")
+        heightelement.textContent = `Height: ${height}`;
+
     }
     catch (error) {
         console.log(error);
     }
+}
+
+const typedTextSpan = document.querySelector(".typed-text");
+const cursor = document.querySelector(".cursor");
+
+const words = ["Fun", "Cool", "Awesome","Life"];
+const typingDelay = 200;
+const erasingDelay = 200;
+const newLetterDelay = 2000;
+let index = 0;
+let charIndex = 0;
+
+document.addEventListener("DOMContentLoaded", () => {
+  if (words.length) {
+    setTimeout(type, newLetterDelay);
+  }
+});
+
+function type() {
+  if (charIndex < words[index].length) {
+    typedTextSpan.textContent += words[index].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } else {
+    setTimeout(erase, newLetterDelay);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    typedTextSpan.textContent = words[index].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } else {
+    index++;
+    if (index >= words.length) {
+      index = 0;
+    }
+    setTimeout(type, typingDelay + 1100);
+  }
 }
